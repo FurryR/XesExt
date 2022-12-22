0f6ee3a2bc995b4a8f4c9d063a43953d4a3f4a39
凌 <nu11ptr@foxmail.com>
2022-12-22 10:22:38 +0800

    Update xesext.js

--- xesext.js
+++ xesext.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         XesExt
 // @namespace    http://github.com/FurryR/XesExt
-// @version      0.1
+// @version      0.1.1
 // @description  Much Better than Original
 // @author       凌
 // @match        https://code.xueersi.com/*
@@ -18,11 +18,11 @@ function getScratchlink(id, version, type) {
   if (type == 'scratch') {
     ret = `https://code.xueersi.com/scratch/index.html?pid=${id}&version=${version}&env=community`
     if (version == '2.0') {
-      if (id.toString().indexOf('8080') != -1) {
+      if (id.indexOf('8080') != -1) {
         ret = `http://dev-code.xueersi.com/scratch/index.html?pid=${id}&version=${version}&env=community`
       }
     } else {
-      if (id.toString().indexOf('8080') != -1) {
+      if (id.indexOf('8080') != -1) {
         ret = `http://dev-code.xueersi.com/scratch3/index.html?pid=${id}&version=${version}&env=community`
       } else {
         ret = `https://code.xueersi.com/scratch3/index.html?pid=${id}&version=${version}&env=community`
@@ -54,10 +54,11 @@ function searchElem(elem, content) {
 (function () {
   'use strict';
   window.addEventListener('load', () => {
+    console.warn('XesExt init')
     /// 变更改编按钮行为 by 凌
     const adaptButton = document.getElementsByClassName('adapt')
     if (adaptButton.length == 1) {
-      console.log('XesExt captured adapt button')
+      console.warn('XesExt captured adapt button')
       adaptButton[0].replaceWith(adaptButton[0].cloneNode(true))
       adaptButton[0].childNodes[1].data = ' 审查 '
       adaptButton[0].addEventListener('click', (ev) => {
@@ -67,7 +68,7 @@ function searchElem(elem, content) {
     } else {
       const notAllowAdaptButton = document.getElementsByClassName('tooltip')
       if (notAllowAdaptButton.length == 1) {
-        console.log('XesExt captured disabled adapt button')
+        console.warn('XesExt captured disabled adapt button')
         notAllowAdaptButton[0].replaceWith(notAllowAdaptButton[0].cloneNode(true))
         notAllowAdaptButton[0].childNodes[0].className = 'adapt'
         notAllowAdaptButton[0].childNodes[0].childNodes[0].className = 'never-adapt'
@@ -92,37 +93,37 @@ function searchElem(elem, content) {
     /// 删除社区公约 by 凌
     const rule = document.getElementsByClassName('rule')
     if (rule.length == 1) {
-      console.log('XesExt captured rule button')
+      console.warn('XesExt captured rule button')
       rule[0].remove()
     }
     /// (不稳定)删除猫博士的开眼课堂和老师们的作品 by 凌
     const filter1 = searchElem('span', '猫博士的开眼课堂')
     const filter2 = searchElem('span', '老师们的作品')
     if (filter1.length == 1) {
-      console.log('XesExt captured recommended-list span1')
+      console.warn('XesExt captured recommended-list span1')
       filter1[0].parentNode.parentNode.parentNode.remove()
     }
     if (filter2.length == 1) {
-      console.log('XesExt captured recommended-list span2')
+      console.warn('XesExt captured recommended-list span2')
       filter2[0].parentNode.parentNode.parentNode.remove()
     }
     /// [编辑器模式] 去除模板按钮 by 凌
     const btn = searchElem('a', ' 模板 ')
     if (btn.length == 1) {
-      console.log('XesExt captured template btn')
+      console.warn('XesExt captured template btn')
       btn[0].remove()
     }
     /// [编辑器模式] 去除编程百科按钮 by 凌
     const wiki = searchElem('a', ' 编程百科 ')
     if (wiki.length == 1) {
-      console.log('XesExt captured wiki btn')
+      console.warn('XesExt captured wiki btn')
       wiki[0].remove()
     }
     /// 删除或替换字体大小按钮 by 凌
     /// 注：在编辑器模式下，字体大小按钮将被替换为主题按钮。
     const textsize = document.getElementsByClassName('btn-font-size')
     if (textsize.length == 1) {
-      console.log('XesExt captured textsize div')
+      console.warn('XesExt captured textsize div')
       if (window.aceEditor && window.location.pathname.startsWith('/ide/code/')) {
         for (const d of textsize[0].childNodes) {
           textsize[0].removeChild(d)
@@ -130,9 +131,9 @@ function searchElem(elem, content) {
         textsize[0].replaceWith(textsize[0].cloneNode(true))
         textsize[0].textContent = ' T '
         textsize[0].addEventListener('click', (ev) => {
-          const t = prompt(`当前主题:${window.aceEditor.getTheme()}\n请输入新的主题 ID(比如 ace/theme/tomorrow_night):`)
+          const t = prompt(`输入新的主题 ID(比如 ace/theme/tomorrow_night):`, window.aceEditor.getTheme())
           if (t) {
-            console.log(`XesExt Switch theme to ${t}`)
+            console.warn(`XesExt Switch theme to ${t}`)
             window.localStorage.setItem('__xesext_theme', t)
             window.aceEditor.setTheme(t)
           }
@@ -145,24 +146,24 @@ function searchElem(elem, content) {
     /// [独占][Pro][编辑器模式] 更换主题 by 凌
     const editor = document.getElementsByClassName('ace-editor tile is-child box ace_editor ace-tm')
     if (editor.length == 1) {
-      console.log('XesExt captured editor div')
+      console.warn('XesExt captured editor div')
       editor[0].attributes.removeNamedItem('data-v-34180700')
       editor[0].className = 'ace-editor tile is-child ace_editor ace-tm'
     }
     const gutterLayer = document.getElementsByClassName('ace_layer ace_gutter-layer')
     if (gutterLayer.length == 1) {
-      console.log('XesExt captured gutter-layer div')
+      console.warn('XesExt captured gutter-layer div')
       gutterLayer[0].className = gutterLayer[0].className.replace('ace_gutter-layer', '')
     }
     // 从 localStorage 读入
     if (window.aceEditor) {
-      console.log('XesExt captured ace editor')
+      console.warn('XesExt captured ace editor')
       const g = window.localStorage.getItem('__xesext_theme')
       if (g) {
-        console.log(`XesExt Switch theme to ${g}`)
+        console.warn(`XesExt Switch theme to ${g}`)
         window.aceEditor.setTheme(g)
       } else {
-        console.log('XesExt Switch theme to ace/theme/tomorrow_night')
+        console.warn('XesExt Switch theme to ace/theme/tomorrow_night')
         window.localStorage.setItem('__xesext_theme', 'ace/theme/tomorrow_night')
         window.aceEditor.setTheme('ace/theme/tomorrow_night')
       }
@@ -172,10 +173,49 @@ function searchElem(elem, content) {
     /// 由于 XesExt 未获得官方允许，删除此段则代表您不同意我们的使用协议。请勿删除此段。
     const footer = document.getElementsByClassName('footer__component')
     if (footer.length == 1) {
-      console.log(`XesExt captured footer__component`)
+      console.warn('XesExt captured footer__component')
       footer[0].innerHTML = '您正在使用 XesExt，一个修改版的学而思。<br>您可于 https://github.com/FurryR/XesExt 获得插件更新。<br>XesExt 是第三方组件，请自行承担使用后果。'
     }
+    /// 更好的反跟踪 by 凌
+    if (window.logger) {
+      console.warn('XesExt captured object logger')
+      for (const v in window.logger) {
+        window.logger[v] = () => {}
+      }
+    }
+    if (window.__XES_LOG__) {
+      console.warn('XesExt captured object __XES_LOG__')
+      for (const v in window.__XES_LOG__) {
+        window.__XES_LOG__[v] = () => {}
+      }
+    }
+    if (window.XesInstance) {
+      console.warn('XesExt captured object XesInstance')
+      for (const v in window.XesInstance) {
+        window.XesInstance[v] = () => {}
+      }
+    }
+    if (window.XesLoggerSDK) {
+      console.warn('XesExt captured fn window.XesLoggerSDK')
+      window.XesLoggerSDK = () => {}
+    }
     /// 初始化完成
-    console.log('XesExt post-init')
+    console.warn('XesExt post-init')
   })
+  /// 更好的反跟踪 提前初始化 by 凌
+  console.warn('XesExt pre-init')
+  console.warn('XesExt captured object XMLHttpRequest')
+  const _open = window.XMLHttpRequest.prototype.open
+  window.XMLHttpRequest.prototype.open = function (e, t, n) {
+    this.__xes_url = t
+    _open.call(this, e, t, n)
+  }
+  const _send = window.XMLHttpRequest.prototype.send
+  window.XMLHttpRequest.prototype.send = function (body) {
+    // console.warn(this)
+    if (this.__xes_url.includes('dj.xesimg.com')) {
+      this.onreadystatechange = () => {}
+      console.warn('XesExt captured dj.xesimg.com XHR')
+    } else _send.call(this, body)
+  }
 })();
